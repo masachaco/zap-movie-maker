0274b78a73d34a9abdd7d58d22ac7fc3f8ca8db4
Masa_Charcoal <4120818+masachaco@users.noreply.github.com>
2022-09-22 04:00:07 +0900

    configを追加

--- zap_movie_maker/__init__.py
+++ zap_movie_maker/__init__.py
@@ -1,4 +1,5 @@
-from voicevox_options import *
-from voiceroid2_options import *
-from clip import *
+from voicevox_options import VoicevoxOptions
+from voiceroid2_options import Voiceroid2Options
+from clip import Clip,TelopOptions,CharacterVoice,CharacterImageSettings,CharacterImageStyle
+
 __version__ = "0.0.1"
